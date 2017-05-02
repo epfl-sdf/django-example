91343c0114c760165bff06c7c0503f7a58923b8b
Christian Zufferey <christian@zufferey.com>
2017-05-02 15:46:28 +0200

    - modifié le script install_all.sh afin d'avoir tout installé en une fois - changé le password de l'ACB, mis le défaut de la SDF

--- install_all.sh
+++ install_all.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 #Installation de l'application
-#zf170421.1035
+#zf170502.1534
 
 echo ------------ start install
-echo ------------ apt-get update
-sudo apt-get update
-
-echo ------------ apt-get install utils
-sudo apt-get install -y gnupg2 jq
-
-echo ------------ secrets uncrypt
-./acb_uncrypt.sh
-
-echo ------------ apt-get install python3
-sudo apt-get install -y python3 python3-pip
-
-echo ------------ install virtualenv
-sudo pip3 install virtualenv
-virtualenv ./.venv
-source ./.venv/bin/activate
-
-echo ------------ install DJANGO requirements
-pip install -r requirements/base.txt
+./install_django.sh
+./install_mysql.sh
+./install_python.sh
