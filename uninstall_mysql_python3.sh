607bc1dbf84df2b69a87be80b5ed45d517919cc3
Christian Zufferey <christian@zufferey.com>
2017-09-06 15:59:30 +0200

    - bien amélioré la désinstallation du binz en cas de réinstallation ;-)

--- uninstall_mysql_python3.sh
+++ uninstall_mysql_python3.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-#juste on test ;-)
-#zf170421.1035
+#juste pour déinstaller quand ça n'a pas marché du premier et que l'on veut rejouer le script install_all.sh
+#zf170906.1554
 
 sudo apt-get --purge -y remove mysql-server mysql-common
-sudo apt-get --purge -y remove python3 python3-pip
+#sudo apt-get --purge -y remove python3 python3-pip
 
-sudo apt-get -y autoremove
+echo ---------- clean install
+echo ---------- clean
 sudo apt-get -y clean
+echo ---------- autoclean
 sudo apt-get -y autoclean
+echo ---------- autoremove
+sudo apt-get -y autoremove --purge
+echo ---------- install
+sudo apt-get -f install               #termine l'installa  si problème dû à l'arrêt du service apt-daily
+echo ---------- clean
+sudo apt-get -y clean
+echo ---------- autoclean
+sudo apt-get -y autoclean
+echo ---------- autoremove
+sudo apt-get -y autoremove --purge
+
 
 sudo rm -R /etc/mysql/
 sudo rm -R /etc/dbconfig-common/
