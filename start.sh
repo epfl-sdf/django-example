f8105a510f3df2138cfb768d7268863f912ed838
Christian Zufferey <christian@zufferey.com>
2017-10-01 17:30:39 +0200

    - ajouté le coup du screen dans le start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #Démarrage de l'application
-#zf170420.1512
+#zf171001.1730
 
 #python django_example/manage.py runserver
+
+echo -e " 
+Afin de garder le serveur WEB permanent, il serait bien de le faire tourner dans un 'screen' avec:
+screen -S django      pour entrer dans screen
+./start.sh            pour lancer le serveur WEB dans screen
+CTRL+a,d              pour sortir de screen en laissant tourner le serveur
+screen -r django      pour revenir dans screen
+screen -x django      pour revenir à plusieurs dans screen
+CTRL+d                pour terminer screen
+screen -list          pour lister tous les screens en fonctionement
+"
 
 source ./.venv/bin/activate
 python django_example/manage.py runserver 0.0.0.0:8000
